99ceeeceba0b99a365dd2b431a7dac3970231af2
Ricardo <ricardonascimento1506@gmail.com>
2026-03-30 21:02:08 -0300

    add model Task

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,25 +1,42 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma.service'
 import { ProjectRequestDTO } from './projects.dto'
 
 @Injectable()
 export class ProjectsService {
+  constructor(private readonly prisma: PrismaService) {}
   findAll() {
-    return ['teste1', 'teste2']
+    return this.prisma.project.findMany()
   }
 
   findById(id: string) {
-    return 'teste1'
+    return this.prisma.project.findFirst({
+      where: {
+        id
+      }
+    })
   }
 
   create(data: ProjectRequestDTO) {
-    return 'created test1'
+    return this.prisma.project.create({
+      data
+    })
   }
 
   update(id: string, data: ProjectRequestDTO) {
-    return 'update teste1'
+    return this.prisma.project.update({
+      where: {
+        id
+      },
+      data
+    })
   }
 
   remove(id: string) {
-    return 'teste1 deleted'
+    return this.prisma.project.delete({
+      where: {
+        id
+      }
+    })
   }
 }
